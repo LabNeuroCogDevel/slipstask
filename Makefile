26b58f91fec65141bf51235411351798fe1cd6fa
Will Foran <willforan@gmail.com>
2020-05-27 10:31:07 -0400

    ✨ analysis: add res.tsv and scripts to get it

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
+.PHONY: res
+
 static/js/utils.js: static/js/utils.ts
 	tsc -m es2015 --lib esnext,dom $<
+
+res: analysis/txt/res.tsv
+analysis/txt/res.tsv: 
+	 analysis/fetch.bash > $@
---
+++ analysis/fetch.bash
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -euo pipefail
+trap 'e=$?; [ $e -ne 0 ] && echo "$0 exited in error"' EXIT
+
+#
+# 20200527WF/FC - init
+#   fetch data from DB
+#   uses DATABASE_URL, or heroku to fetch
+#
+
+# check/try to fetch database location
+! env| grep -q ^DATABASE_URL= && DATABASE_URL=""
+[ -z "$DATABASE_URL" ] && command -v heroku && DATABASE_URL=$(heroku config:get DATABASE_URL)
+[ -z "$DATABASE_URL" ] && echo "missing DATABASE_URL cannot fetch data! fix: export DATABASE_URL=\$(heroku config:get DATABASE_URL)" && exit 1
+
+columns="block trial_index time_elapsed trial_type internal_node_id stim key_press rt outcome valued devalued cor_dir isdevalued chose score"
+jsoncol=$(echo "$columns"|sed 's/^\| /&./g;s/ /, /g;')
+# .block, .trial_index, ...
+
+# header
+echo "subjID $columns" | sed 's/ /\t/g'
+
+# parse nested json
+# TODO: get codeversion into jq
+psql "$DATABASE_URL" -AtF$'\t' -c  'select datastring from slips' |
+ jq -r '
+  . as $r |
+  .data[0].trialdata.responses as $q |
+  .data[] |
+  .trialdata |
+  select(.score != null) |
+  [$r.workerId, '"$jsoncol"'] |
+  @tsv'
