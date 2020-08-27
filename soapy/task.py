cd27249349bbeba677a69e354bfe1a6af791c83c
Will Foran <willforan@gmail.com>
2020-08-27 08:13:30 -0400

    🐛 do not append, just replace save_path

--- soapy/task.py
+++ soapy/task.py
@@ -537,7 +537,7 @@ class FabFruitTask:
         if not self.save_path:
             print(f"WARNING: trying to save buth no 'save_path' given")
             return
-        self.events.saveAsText(self.save_path)
+        self.events.saveAsText(self.save_path, appendFile=False)
 
     def instruction(self, top: str, func, bottom="(push any key)", flip=True) -> Keypress:
         """print some text and run an arbitraty function"""
