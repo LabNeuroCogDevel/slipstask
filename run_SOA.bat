a2ae71299f7224243793c97d7b0834e04f37c3d4
Will Foran <willforan@gmail.com>
2020-09-09 08:13:08 -0400

    🎨 [py/windows] .bat w/ var paths + check deps

--- run_SOA.bat
+++ run_SOA.bat
@@ -1,7 +1,18 @@
-echo "running SOAMR"
-"C:\Program Files\PsychoPy3\python.exe" C:\\Users\\Luna\\Desktop\\slipstask\\soapy\\bin\\SOAMR
+@echo off
+SET PY="L:\Applications\PsychoPy3\python.exe"
+SET SOA=L:\\Tasks\\slipstask\\soapy
+
+echo check depends
+@echo on
+%PY% -c "import soapy" || %PY% -m pip install -e "%SOA%"
+
+echo running SOAMR
+%PY% "%SOA%\\bin\\SOAMR"
+
+
+@echo off
 echo finished
-echo
-echo
-echo "PUSH ANY KEY TO CLOSE"
+echo.
+echo.
+echo PUSH ANY KEY TO CLOSE
 pause
