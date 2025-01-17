c1bfc45af97933ed80d2e8e81108158083067b90
WillForan <willforan@gmail.com>
2025-01-17 10:55:13 -0500

    🚧 timing files in LFS

--- soa_DD.py
+++ soa_DD.py
@@ -4,7 +4,7 @@ from soapy import read_img_list
 from soapy.task import FabFruitTask
 from soapy.info import FabFruitInfo
 win = visual.Window([800, 600])
-info = FabFruitInfo(timing_files=['timing/seeded/1_445/1350647090/DD.csv'])
+info = FabFruitInfo(timing_files=['soapy/timing/DD/6/1135626388.csv'])
 info.set_names(read_image_list('fruits'))
 task = FabFruitTask(win, info)
 task.run()
